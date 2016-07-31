bc6a8e168410d161ffe4a4535427957d4496ac9b
chistery <merthedil@gmail.com>
2016-07-31 18:17:30 -0500

    Added PUT functionality.

--- server.js
+++ server.js
@@ -30,6 +30,17 @@ Storage.prototype.delete = function(target_id) {
     return deleted;
 };
 
+Storage.prototype.replace = function(target_id, replacement_obj) {
+    var replaced = [];
+    this.items = this.items.map(function(obj) {
+        if (obj.id === target_id) {
+        	replaced.push(obj);
+            obj === replacement_obj;
+        }
+    });
+    return replaced;
+};
+
 var storage = new Storage();
 storage.add('Broad beans');
 storage.add('Tomatoes');
@@ -67,6 +78,30 @@ app.delete('/items/:id', jsonParser, function(req, res) {
     } else {
         res.status(200).json({
             status: "No items were deleted from the shopping list. Chances are this ID has already been deleted."
+        });
+    }
+});
+
+app.put('/items/:id', jsonParser, function(req, res) {
+    // Need to force the string 'id' to a Number for proper use in an Array reference
+    var passed_obj = req.body;
+    var inner_id = Number(req.params.id);
+    if (isNaN(inner_id)) {
+        return res.status(400).json({
+            "error": "Requested ID '"+req.params.id+"' is not a number."
+        });
+    }
+    var replaced = storage.replace(inner_id, passed_obj);
+    if (replaced.length > 0) {
+        res.status(200).json({
+            status: "Successfully replaced "+replaced.length+" item(s).",
+            replaced: replaced
+        });
+    } else {
+    	storage.add(passed_obj);
+        res.status(200).json({
+            status: inner_id+" did not appear as a valid ID in original list; appended to the shopping list.",
+            added: passed_obj
         });
     }
 });

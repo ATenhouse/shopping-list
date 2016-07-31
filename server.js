9ce3b462d7bcebb1c82710df9f1b0ef5ccbe0124
chistery <merthedil@gmail.com>
2016-07-31 16:22:31 -0500

    Added DELETE logic

--- server.js
+++ server.js
@@ -17,6 +17,19 @@ Storage.prototype.add = function(name) {
     return item;
 };
 
+Storage.prototype.delete = function(target_id) {
+    var deleted = [];
+    this.items = this.items.filter(function(obj) {
+        if (obj.id !== target_id) {
+            return true;
+        } else {
+            deleted.push(obj);
+            return false;
+        }
+    });
+    return deleted;
+};
+
 var storage = new Storage();
 storage.add('Broad beans');
 storage.add('Tomatoes');
@@ -33,9 +46,29 @@ app.post('/items', jsonParser, function(req, res) {
     if (!req.body) {
         return res.sendStatus(400);
     }
-    console.log(req.body);
-    var item = storage.add(req.body.name);
+    var item = storage.add(req.body.id);
     res.status(201).json(item);
+});
+
+app.delete('/items/:id', jsonParser, function(req, res) {
+    // Need to force the string 'id' to a Number for proper use in an Array reference
+    var inner_id = Number(req.params.id);
+    if (isNaN(inner_id)) {
+        return res.status(400).json({
+            "error": "Requested ID '"+req.params.id+"' is not a number."
+        });
+    }
+    var deleted_items = storage.delete(inner_id);
+    if (deleted_items !== null) {
+        res.status(200).json({
+            status: "Successfully deleted some items.",
+            items: deleted_items
+        });
+    } else {
+        res.status(200).json({
+            status: "No items were deleted from the shopping list. Chances are this ID has already been deleted."
+        });
+    }
 });
 
 app.listen(process.env.PORT || 8080);
